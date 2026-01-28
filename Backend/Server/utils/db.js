bf2d876b8b5dea01cffdc08d5381da4ba08f3245
Omkar Kardile <omkardile84@gmail.com>
2026-01-28 21:56:17 +0530

    Update CORS package and database config, enable CORS

--- Backend/Server/utils/db.js
+++ Backend/Server/utils/db.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2')
 
 const pool = mysql.createPool({
   host: 'localhost',
-  user: 'W3_93113_Tanmay',
-  password: 'manager',
-  database: 'car_service_database'
+  user: 'w3-93109',
+  password: 'omkarkar',
+  database: 'car_service_station_database'
 })
 
 module.exports = pool
